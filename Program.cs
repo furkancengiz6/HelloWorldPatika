21a67a3fecb2a730616f640e7e0a2c9d07d5a7d7
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2024-12-16 11:12:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldPatika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Furkan Cengiz"+"\n" + "Patika'nın tecrübeleri işiğinda sektore daha hızlı ve saglam giris yapmak icın bu bootcampteyim!");
+          
+        }
+    }
+}
